caff85d67e20e5553c14b3158dd1f30329d6d355
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-23 19:42:53 +0200

    Add trader camp layout model

---
+++ ImmersiveTrader/Models/TraderCampDefinition.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace ImmersiveTrader.Models;
+
+public sealed record TraderCampProp(string Prefab, Vector3 Position, Vector3 Rotation, float Scale = 1f);
+
+public sealed record TraderCampDefinition(string TraderId, TraderCampProp[] Props);
