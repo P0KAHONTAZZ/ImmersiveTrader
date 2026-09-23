4bfe538bedb6e4ee1048461bf4a215eaf03d591e
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-23 16:31:33 +0200

    Implement current Hoverable offset contract

--- ImmersiveTrader/Components/TraderNpc.cs
+++ ImmersiveTrader/Components/TraderNpc.cs
@@ -12,22 +12,19 @@ public sealed class TraderNpc : MonoBehaviour, Hoverable, Interactable
         TraderRegistry.Traders.FirstOrDefault(x => x.Id == TraderId);
 
     public string GetHoverName() => Definition?.Name ?? "Trader";
+    public Vector3 GetHoverOffset() => Vector3.up * 1.8f;
 
     public string GetHoverText()
     {
         if (Definition == null) return string.Empty;
-
         if (TraderId == "midka")
             return $"{Definition.Name}\n[<color=yellow><b>$KEY_Use</b></color>] Talk\nWar healer & field medic";
-
         return $"{Definition.Name}\n[<color=yellow><b>$KEY_Use</b></color>] Talk";
     }
 
     public bool Interact(Humanoid user, bool hold, bool alt)
     {
-        if (hold || user is not Player player || Definition == null)
-            return false;
-
+        if (hold || user is not Player player || Definition == null) return false;
         TraderInteraction.Handle(player, Definition, alt);
         return true;
     }
