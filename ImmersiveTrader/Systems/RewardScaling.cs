8d03aa5dc11d47a369153b3a52657436de86e60c
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-23 17:14:35 +0200

    Add physical distance bonus alongside biome tier scaling

--- ImmersiveTrader/Systems/RewardScaling.cs
+++ ImmersiveTrader/Systems/RewardScaling.cs
@@ -4,10 +4,10 @@ namespace ImmersiveTrader;
 
 public static class RewardScaling
 {
-    public static int GetMultiplier(int sourceBiomeTier, int targetBiomeTier)
+    public static int GetBiomeMultiplier(int sourceBiomeTier, int targetBiomeTier)
     {
-        int distance = Math.Abs(sourceBiomeTier - targetBiomeTier);
-        return distance switch
+        int tierDistance = Math.Abs(sourceBiomeTier - targetBiomeTier);
+        return tierDistance switch
         {
             0 => 1,
             1 => 2,
@@ -16,6 +16,18 @@ public static class RewardScaling
         };
     }
 
-    public static int GetRewardAmount(int baseAmount, int sourceBiomeTier, int targetBiomeTier)
-        => baseAmount * GetMultiplier(sourceBiomeTier, targetBiomeTier);
+    public static float GetDistanceMultiplier(float metres)
+    {
+        if (metres < 1500f) return 1f;
+        if (metres < 3000f) return 1.25f;
+        if (metres < 5000f) return 1.5f;
+        if (metres < 7500f) return 1.75f;
+        return 2f;
+    }
+
+    public static int GetRewardAmount(int baseAmount, int sourceBiomeTier, int targetBiomeTier, float metres)
+        => Math.Max(1, (int)Math.Round(baseAmount * GetBiomeMultiplier(sourceBiomeTier, targetBiomeTier) * GetDistanceMultiplier(metres)));
+
+    public static int GetMultiplier(int sourceBiomeTier, int targetBiomeTier)
+        => GetBiomeMultiplier(sourceBiomeTier, targetBiomeTier);
 }
