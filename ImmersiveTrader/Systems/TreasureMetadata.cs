6131fbb761838e261e286026dbe0b16fdba6846d
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-23 17:14:19 +0200

    Store source trader world position on shipments

--- ImmersiveTrader/Systems/TreasureMetadata.cs
+++ ImmersiveTrader/Systems/TreasureMetadata.cs
@@ -8,12 +8,25 @@ public static class TreasureMetadata
     private const string SourceTraderKey = "ImmersiveTrader.SourceTrader";
     private const string SourceTierKey = "ImmersiveTrader.SourceTier";
     private const string ShipmentIdKey = "ImmersiveTrader.ShipmentId";
+    private const string SourceXKey = "ImmersiveTrader.SourceX";
+    private const string SourceZKey = "ImmersiveTrader.SourceZ";
 
-    public static void Stamp(ItemDrop.ItemData item, string sourceTraderId, int sourceBiomeTier)
+    public static void Stamp(ItemDrop.ItemData item, string sourceTraderId, int sourceBiomeTier, float sourceX = 0f, float sourceZ = 0f)
     {
         item.m_customData[SourceTraderKey] = sourceTraderId;
         item.m_customData[SourceTierKey] = sourceBiomeTier.ToString(CultureInfo.InvariantCulture);
         item.m_customData[ShipmentIdKey] = Guid.NewGuid().ToString("N");
+        item.m_customData[SourceXKey] = sourceX.ToString(CultureInfo.InvariantCulture);
+        item.m_customData[SourceZKey] = sourceZ.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public static bool TryGetSourcePosition(ItemDrop.ItemData item, out float x, out float z)
+    {
+        x = 0f; z = 0f;
+        return item.m_customData.TryGetValue(SourceXKey, out var sx)
+            && item.m_customData.TryGetValue(SourceZKey, out var sz)
+            && float.TryParse(sx, NumberStyles.Float, CultureInfo.InvariantCulture, out x)
+            && float.TryParse(sz, NumberStyles.Float, CultureInfo.InvariantCulture, out z);
     }
 
     public static string GetShipmentId(ItemDrop.ItemData item)
