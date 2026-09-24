ec0cb273797a6978f49edd6e3ebc287854aa2c05
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-24 03:16:29 +0200

    Resolve ambiguous ItemData GetTooltip patch before NPC registration

--- ImmersiveTrader/Patches/ContractTooltipPatch.cs
+++ ImmersiveTrader/Patches/ContractTooltipPatch.cs
@@ -1,11 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 
 namespace ImmersiveTrader.Patches;
 
 /// <summary>Adds live contract progress to the vanilla item tooltip without custom UI.</summary>
-[HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetTooltip))]
+[HarmonyPatch]
 internal static class ContractTooltipPatch
 {
+    // Valheim exposes several GetTooltip overloads. An unqualified HarmonyPatch
+    // fails during Plugin.Awake and prevents all trader NPCs from registering.
+    private static IEnumerable<MethodBase> TargetMethods() =>
+        typeof(ItemDrop.ItemData)
+            .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(method => method.Name == nameof(ItemDrop.ItemData.GetTooltip) && method.ReturnType == typeof(string));
+
     private static void Postfix(ItemDrop.ItemData __instance, ref string __result)
     {
         string progress = ContractMetadata.GetProgressText(__instance);
