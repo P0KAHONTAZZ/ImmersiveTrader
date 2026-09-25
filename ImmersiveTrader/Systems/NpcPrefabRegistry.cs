f3156f48d2ac05119f2b95523f2bed26608d0593
P0KAHONTAZZ <toxno1@gmail.com>
2026-09-25 19:44:22 +0200

    Mount visual-only troll outfit on Troldad shell

--- ImmersiveTrader/Systems/NpcPrefabRegistry.cs
+++ ImmersiveTrader/Systems/NpcPrefabRegistry.cs
@@ -117,6 +117,7 @@ public static class NpcPrefabRegistry
         if (traderId == "troldad")
         {
             shell.transform.localScale = Vector3.one * visualScale;
+            shell.AddComponent<TroldadVisualProps>();
             MakePassive(shell);
             PrepareCreatureTrader(shell, npc);
         }
